34ebcbb884fcedcc33b155448f5961f123934eb0
Morteza <mortezajafari76@gmail.com>
2026-06-28 15:23:47 +0330

    feat: add NoResult component and corresponding SVG asset for improved user feedback in job listings

---
+++ app/components/Elements/NoResult.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="py-12 text-center" :class="wrapperClass">
+    <Icon name="svg:no-result" :size="iconSize" />
+    <h5 class="font-yb-bold text-lg text-text-tertiary">{{ title }}</h5>
+    <span class="text-sm text-text-passive">{{ description }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    title?: string
+    description?: string
+    iconSize?: number
+    wrapperClass?: string
+  }>(),
+  {
+    title: 'فرصت شغلی موجود نیست',
+    description: 'متاسفانه هیچ فرصت شغلی جهت نمایش موجود نیست!',
+    iconSize: 240,
+    wrapperClass: '',
+  },
+)
+</script>
