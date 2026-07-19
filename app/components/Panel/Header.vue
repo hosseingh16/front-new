9fcb3f6a60bedcb51c95ef8288c02870013ec298
Morteza <mortezajafari76@gmail.com>
2026-07-19 14:34:28 +0330

    refactor: streamline Header.vue by consolidating logo link structure and updating user panel text for improved clarity and organization

--- app/components/Panel/Header.vue
+++ app/components/Panel/Header.vue
@@ -1,29 +1,30 @@
 <template>
   <header
-    class=" fixed right-0 left-0 z-20 h-16 px-4 shadow-sm flex justify-between items-center bg-white max-md:border-b-2 max-md:border-gray-default"
+    class="fixed right-0 left-0 z-20 h-16 px-4 shadow-sm flex justify-between items-center bg-white max-md:border-b-2 max-md:border-gray-default"
   >
     <div class="flex gap-12">
       <div class="flex gap-2 items-center">
         <!-- <div class="lg:hidden">
           <Icon name="svg:menu" size="20" />
         </div> -->
-        <NuxtLink to="/">
+        <NuxtLink to="/" class="flex items-center">
           <NuxtImg src="/images/logo.png" alt="Logo" class="lg:w-10 lg:h-12" />
+
+          <div class="max-lg:hidden">
+            <h1 class="font-semibold">های‌حساب</h1>
+            <h2 class="text-caption">خانه حسابداران با تجربه</h2>
+          </div>
         </NuxtLink>
-        <div class="max-lg:hidden">
-          <h1 class="font-semibold">های‌حساب</h1>
-          <h2 class="text-caption">خانه حسابداران با تجربه</h2>
-        </div>
         <div class="flex items-center gap-2 max-lg:hidden mr-8">
           <NuxtLink to="" class="text-sm hover:opacity-80">
             <Icon name="svg:panel-right" size="16" />
           </NuxtLink>
           <span class="text-gray-default">|</span>
-          <NuxtLink to="" class="text-sm hover:opacity-80">
+          <NuxtLink to="/" class="text-sm hover:opacity-80">
             <Icon name="svg:home" size="16" />
           </NuxtLink>
           <span class="text-gray-default">/</span>
-          <span class="text-text-muted text-caption">عنوان</span>
+          <span class="text-text-muted text-caption">پنل کاربری</span>
         </div>
       </div>
     </div>
