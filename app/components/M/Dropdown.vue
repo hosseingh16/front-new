6f420d75bf495e5f346f9f34882c32fbca3d51af
Morteza <mortezajafari76@gmail.com>
2026-07-22 15:09:43 +0330

    feat: add new success color variables to tailwind.css for enhanced styling options; update Dropdown component to replace image with icon for improved visual consistency

--- app/components/M/Dropdown.vue
+++ app/components/M/Dropdown.vue
@@ -10,7 +10,12 @@
       class="dropdown-content left-0 z-1 min-w-62 w-fit rounded-xl border-3 border-gray-default bg-surface-50 p-0 font-semibold shadow-sm"
       :class="hover ? 'mt-0' : 'mt-2'"
     >
-      <img src="/images/rectangle.png" class="absolute -top-2 left-5" />
+      <!-- <img src="/images/rectangle.png" class="absolute -top-2 left-5" /> -->
+      <Icon
+        name="tabler:triangle-filled"
+        size="12"
+        class="absolute -top-3 left-5 text-surface-200"
+      />
       <slot name="content" />
     </div>
   </div>
