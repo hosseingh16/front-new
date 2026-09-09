008ca934c656c60fad873fcdba0797712f3a4114
Morteza <mortezajafari76@gmail.com>
2026-09-09 13:36:52 +0330

    feat: conditionally render GoToTop component based on route; add computed property to determine if current route is dashboard

--- app/app.vue
+++ app/app.vue
@@ -2,5 +2,11 @@
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
-  <GoToTop />
+  <GoToTop v-if="!isDashboard" />
 </template>
+
+<script setup lang="ts">
+const route = useRoute();
+
+const isDashboard = computed(() => route.path.startsWith("/dashboard"));
+</script>
